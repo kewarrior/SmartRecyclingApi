79d9821b8b13e608b8adc116614ad6a5a721774d
kewarrior <joao_pinho96@hotmail.com>
2026-07-18 09:10:43 +0100

    migratio - Alterar Status Pedido

---
+++ SmartRecyclingApi/Migrations/20260718075443_AlterarStatusPedidoParaEnum.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartRecyclingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarStatusPedidoParaEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status_Pedido",
+                table: "Pedido",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status_Pedido",
+                table: "Pedido",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
